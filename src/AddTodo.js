178024bf2f33c590e4ba15c0e45d8b9671684bf2
Anna Fuentes <annafuentes@hiqstom066.guest.stockholm.se>
2018-06-25 12:20:33 +0200

    Add a new Todo

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import Button from './Button.js';
 
 class AddTodo extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			userInput: ''
+		}
+	}
+
+	onChange(e) {
+		this.setState( {
+			userInput: e.target.value
+		})
+	}
+
+	onClick() {
+		this.props.callback(this.state.userInput)
+		this.setState({
+			userInput: ''
+		})
+	}
+
   render() {
     return (
-      <div>
-        <input />
-        <Button />
+    	<div>
+        <input type="text" value={this.state.userInput} onChange={this.onChange.bind(this)}/>
+        <button onClick={this.onClick.bind(this)}>Add</button>
       </div>
     );
   }
--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import TodoList from './TodoList';
-import './App.css';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			todo: []
+		}
+
+	}
+
+	handleChange(text) {
+		this.setState(state => ({ todo: state.todo.concat(text) }))
+	}
+
   render() {
     return (
       <div>
-        <TodoList />
+      	<AddTodo callback={this.handleChange.bind(this)}/>
+      	<h1>Todo List!</h1>
       </div>
     );
   }
